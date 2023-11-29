878fcb5011bfc11a899cd6c2f6f0dd45d8d1e992
krish-na-7 <36792759+krish-na-7@users.noreply.github.com>
2023-11-29 21:09:37 +0530

    Time: 18 ms (14.05%) | Memory: 39.6 MB (55.50%) - LeetSync

--- 69-sqrtx/sqrtx.java
+++ 69-sqrtx/sqrtx.java
@@ -1,5 +1,15 @@
 class Solution {
     public int mySqrt(int x) {
-        return (int)Math.sqrt(x);
+        // return (int)Math.sqrt(x);
+        int i = 0, o = 1;
+        while(x > 0){
+            x = x - o;
+            if(x < 0){
+                return i;
+            }
+            o = o + 2;
+            i++;
+        }
+        return i;
     }
 }
