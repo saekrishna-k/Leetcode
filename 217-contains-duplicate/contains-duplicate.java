e730ca22643bf780801d7ab58b7ef06181dcc06f
krish-na-7 <36792759+krish-na-7@users.noreply.github.com>
2023-12-01 22:18:54 +0530

    Time: 19 ms (28.14%) | Memory: 58.1 MB (5.94%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.java
+++ 217-contains-duplicate/contains-duplicate.java
@@ -1,21 +1,29 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        // int f = 0;
-        // for(int i = 0 ; i < nums.length ; i++){
-        //    for(int j = i + 1 ; j < nums.length ; j++){
-        //        if(nums[j] == nums[i]){
+        // int l = nums.length;
+        // for(int i = 0 ; i < l - 1; i++){
+        //    for(int j = i + 1 ; j < l ; j++){
+        //        if(nums[i] == nums[j]){
         //            return true;
         //        }
         //    }
         // }
         // return false;
 
-        Set<Integer> s = new HashSet<>();
-        for(int i = 0 ; i < nums.length ; i++){
-           if(!s.add(nums[i])){
-               return true;
-           }
+        Arrays.sort(nums);
+        int n = nums.length;
+        for (int i = 1; i < n; i++) {
+            if (nums[i] == nums[i - 1])
+                return true;
         }
         return false;
+
+        // Set<Integer> s = new HashSet<>();
+        // for(int i = 0 ; i < nums.length ; i++){
+        //    if(!s.add(nums[i])){
+        //        return true;
+        //    }
+        // }
+        // return false;
     }
 }
