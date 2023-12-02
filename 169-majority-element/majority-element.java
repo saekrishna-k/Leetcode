fd81e6163a869f3530497976fb4a181156e5a802
krish-na-7 <36792759+krish-na-7@users.noreply.github.com>
2023-12-02 19:20:48 +0530

    Time: 11 ms (34.72%) | Memory: 46.7 MB (85.38%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -1,22 +1,20 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        Arrays.sort(nums);
-        return nums[nums.length/2];
+        // Arrays.sort(nums);
+        // return nums[nums.length/2];
 
 
-        // int k = nums.length / 2;
-        // int max = 0;
-        // Map<Integer,Integer> m = new HashMap<>();
-        // for(int i = 0 ; i < nums.length ; i++){
-        //     int v = m.getOrDefault(nums[i],0);
-        //     m.put(nums[i],++v);
-        //     System.out.print(nums[i] + " ");
-        //     System.out.print(v);
-        //     System.out.println();
-        //     if(v >= k){
-        //         return nums[i];
-        //     }
-        // }
-        // return 0;
+        int n = nums.length;
+        int x = n / 2;
+        Map<Integer,Integer> m = new HashMap<>();
+        for(int i = 0 ; i < n ; i++){
+            m.put(nums[i], m.getOrDefault(nums[i], 0) + 1);
+        }
+        for(Map.Entry<Integer,Integer> e : m.entrySet()){
+            if(e.getValue() > x){
+                return e.getKey();
+            }
+        }
+        return 0;
     }
 }
