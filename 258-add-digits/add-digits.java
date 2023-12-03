07712c7b473dd1ce31749282ef71e7b5b79f8d17
krish-na-7 <36792759+krish-na-7@users.noreply.github.com>
2023-12-03 09:27:32 +0530

    Time: 0 ms (100.00%) | Memory: 39.9 MB (24.81%) - LeetSync

--- 258-add-digits/add-digits.java
+++ 258-add-digits/add-digits.java
@@ -11,15 +11,22 @@ class Solution {
         // }
         // return s;
 
-        int s = 0;
-        while(num > 0){
-            s = s + num % 10;
-            num /= 10;
-        }
-        if(s < 10){
-            return s;
-        }else{
-            return addDigits(s);
-        }
+        // int s = 0;
+        // while(num > 0){
+        //     s = s + num % 10;
+        //     num /= 10;
+        // }
+        // if(s < 10){
+        //     return s;
+        // }else{
+        //     return addDigits(s);
+        // }
+
+         if(num == 0)
+          return 0;
+         else if(num%9 == 0)
+          return 9;
+         else
+          return num % 9;
     }
 }
