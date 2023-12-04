7a111b76c22fce6bc69b11186f0987b659787498
krish-na-7 <36792759+krish-na-7@users.noreply.github.com>
2023-12-04 19:11:10 +0530

    Time: 1 ms (86.03%) | Memory: 40.2 MB (9.88%) - LeetSync

--- 231-power-of-two/power-of-two.java
+++ 231-power-of-two/power-of-two.java
@@ -2,20 +2,26 @@ class Solution {
     public boolean isPowerOfTwo(int n) {
         // return n > 0 && n != 0 && (n & (n-1)) == 0;
 
-        if(n <= 0){
-            return false;
-        }
+        // if(n <= 0){
+        //     return false;
+        // }
 
-        while(n > 0){
-            if(n == 1){
-            return true;
-            }
-            if(n % 2 != 0){
-                return false;
-            }else{
-                n /= 2;
-            }
-        }
-        return true;
+        // while(n > 0){
+        //     if(n == 1){
+        //     return true;
+        //     }
+        //     if(n % 2 != 0){
+        //         return false;
+        //     }else{
+        //         n /= 2;
+        //     }
+        // }
+        // return true;
+
+
+        if(n==0) return false;
+        while(n%2==0) n/=2;
+        return n==1;
+
     }
 }
