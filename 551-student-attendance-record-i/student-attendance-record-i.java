eafe9859d7bff208405ba407e0a6305830086232
krish-na-7 <36792759+krish-na-7@users.noreply.github.com>
2023-12-08 08:14:08 +0530

    Time: 0 ms (100.00%) | Memory: 40.9 MB (12.24%) - LeetSync

--- 551-student-attendance-record-i/student-attendance-record-i.java
+++ 551-student-attendance-record-i/student-attendance-record-i.java
@@ -1,24 +1,40 @@
 class Solution {
 
     public boolean checkRecord(String s) {
-        int a = 0, l = 0, c = 0;
+        // int a = 0, l = 0, c = 0;
+        // for (int i = 0; i < s.length(); i++) {
+        //     if (s.charAt(i) == 'A') {
+        //         a++;
+        //         l = c > l ? c : l;
+        //         c = 0;
+        //     } else if (s.charAt(i) == 'L') {
+        //         c++;
+        //     } else {
+        //         l = c > l ? c : l;
+        //         c = 0;
+        //     }
+        // }
+        // l = c > l ? c : l;
+        // if (a < 2 && l < 3) {
+        //     return true;
+        // } else {
+        //     return false;
+        // }
+
+        int a = 0, l = 0;
         for (int i = 0; i < s.length(); i++) {
             if (s.charAt(i) == 'A') {
                 a++;
-                l = c > l ? c : l;
-                c = 0;
+                l = 0;
             } else if (s.charAt(i) == 'L') {
-                c++;
+                l++;
             } else {
-                l = c > l ? c : l;
-                c = 0;
+                l = 0;
+            }
+            if (a >= 2 || l >= 3) {
+                return false;
             }
         }
-        l = c > l ? c : l;
-        if (a < 2 && l < 3) {
-            return true;
-        } else {
-            return false;
-        }
+        return true;
     }
 }
