95c774257169215558b706d79d3bc6fbc97fb34d
krish-na-7 <36792759+krish-na-7@users.noreply.github.com>
2023-12-12 07:37:03 +0530

    Time: 1 ms (100.00%) | Memory: 40.6 MB (82.69%) - LeetSync

--- 1823-determine-if-string-halves-are-alike/determine-if-string-halves-are-alike.java
+++ 1823-determine-if-string-halves-are-alike/determine-if-string-halves-are-alike.java
@@ -1,41 +1,55 @@
 class Solution {
 
     public boolean halvesAreAlike(String s) {
-        int i = 0, j = s.length() / 2;
-        int a = 0, b = 0;
-        while (i < s.length() / 2) {
-            if (
-                s.charAt(i) == 'a' ||
-                s.charAt(i) == 'e' ||
-                s.charAt(i) == 'i' ||
-                s.charAt(i) == 'o' ||
-                s.charAt(i) == 'u' ||
-                s.charAt(i) == 'A' ||
-                s.charAt(i) == 'E' ||
-                s.charAt(i) == 'I' ||
-                s.charAt(i) == 'O' ||
-                s.charAt(i) == 'U'
-            ) {
-                a++;
-            }
+        // int i = 0, j = s.length() / 2;
+        // int a = 0, b = 0;
+        // while (i < s.length() / 2) {
+        //     if (
+        //         s.charAt(i) == 'a' ||
+        //         s.charAt(i) == 'e' ||
+        //         s.charAt(i) == 'i' ||
+        //         s.charAt(i) == 'o' ||
+        //         s.charAt(i) == 'u' ||
+        //         s.charAt(i) == 'A' ||
+        //         s.charAt(i) == 'E' ||
+        //         s.charAt(i) == 'I' ||
+        //         s.charAt(i) == 'O' ||
+        //         s.charAt(i) == 'U'
+        //     ) {
+        //         a++;
+        //     }
 
-            if (
-                s.charAt(j) == 'a' ||
-                s.charAt(j) == 'e' ||
-                s.charAt(j) == 'i' ||
-                s.charAt(j) == 'o' ||
-                s.charAt(j) == 'u' ||
-                s.charAt(j) == 'A' ||
-                s.charAt(j) == 'E' ||
-                s.charAt(j) == 'I' ||
-                s.charAt(j) == 'O' ||
-                s.charAt(j) == 'U'
-            ) {
-                b++;
+        //     if (
+        //         s.charAt(j) == 'a' ||
+        //         s.charAt(j) == 'e' ||
+        //         s.charAt(j) == 'i' ||
+        //         s.charAt(j) == 'o' ||
+        //         s.charAt(j) == 'u' ||
+        //         s.charAt(j) == 'A' ||
+        //         s.charAt(j) == 'E' ||
+        //         s.charAt(j) == 'I' ||
+        //         s.charAt(j) == 'O' ||
+        //         s.charAt(j) == 'U'
+        //     ) {
+        //         b++;
+        //     }
+        //     j++;
+        //     i++;
+        // }
+        // return a == b ? true : false;
+
+        int l = s.length() / 2;
+        return count(s.substring(0, l)) == count(s.substring(l));
+    }
+
+    public static int count(String s) {
+        String v = "aeiouAEIOU";
+        int c = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (v.indexOf(s.charAt(i)) != -1) {
+                c++;
             }
-            j++;
-            i++;
         }
-        return a == b ? true : false;
+        return c;
     }
 }
