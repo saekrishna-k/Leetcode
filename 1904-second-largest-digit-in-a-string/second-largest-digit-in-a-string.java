9762cd4689b84968ba9981968884f06ef392cb0b
krish-na-7 <36792759+krish-na-7@users.noreply.github.com>
2023-12-17 12:30:30 +0530

    Time: 1 ms (99.28%) | Memory: 41.2 MB (57.63%) - LeetSync

--- 1904-second-largest-digit-in-a-string/second-largest-digit-in-a-string.java
+++ 1904-second-largest-digit-in-a-string/second-largest-digit-in-a-string.java
@@ -1,19 +1,19 @@
 class Solution {
 
     public int secondHighest(String s) {
-        int m = -1, sm = -1;
+        int l = -1, sl = -1;
         for (int i = 0; i < s.length(); i++) {
             char c = s.charAt(i);
             if (c >= '0' && c <= '9') {
                 int d = c - '0';
-                if (m < d) {
-                    sm = m;
-                    m = d;
-                } else if (sm < d && d < m) {
-                    sm = d;
+                if (l < d) {
+                    sl = l;
+                    l = d;
+                } else if (sl < d && d < l) {
+                    sl = d;
                 }
             }
         }
-        return sm;
+        return sl;
     }
 }
