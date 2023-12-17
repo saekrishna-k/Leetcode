8e8c8c58e79855a371ffb4ff4522e302f1bef05d
krish-na-7 <36792759+krish-na-7@users.noreply.github.com>
2023-12-17 12:25:17 +0530

    Time: 2 ms (75.58%) | Memory: 40.8 MB (90.48%) - LeetSync

--- 1904-second-largest-digit-in-a-string/second-largest-digit-in-a-string.java
+++ 1904-second-largest-digit-in-a-string/second-largest-digit-in-a-string.java
@@ -1,35 +1,19 @@
 class Solution {
 
     public int secondHighest(String s) {
-        // int m = -1, sm = -1;
-        // for (int i = 0; i < s.length(); i++) {
-        //     if (s.charAt(i) > '0' && s.charAt(i) < '9') {
-        //         int n = s.charAt(i) - '0';
-        //         if (m < n) {
-        //             sm = m;
-        //             m = n;
-        //         } else if (sm < n && n < m) {
-        //             sm = n;
-        //         }
-        //     }
-        // }
-        // return sm;
-
-        int first = -1;
-        int second = -1;
-        for (int i=0; i<s.length(); i++) {
-            char current = s.charAt(i);
-            if (Character.isDigit(current)) {
-                int value = current - '0';
-                if (value > first) {
-                    second = first;
-                    first = value;
-                }
-                else if (second < value && value < first) {
-                    second = value;
+        int m = -1, sm = -1;
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (Character.isDigit(c)) {
+                int d = c - '0';
+                if (m < d) {
+                    sm = m;
+                    m = d;
+                } else if (sm < d && d < m) {
+                    sm = d;
                 }
             }
         }
-        return second;
+        return sm;
     }
 }
