89cd05c018d86bbbb3806cb61ed2dc5af14ff052
krish-na-7 <36792759+krish-na-7@users.noreply.github.com>
2023-12-18 20:05:06 +0530

    Time: 0 ms (100.00%) | Memory: 40 MB (69.11%) - LeetSync

--- 2589-maximum-value-of-a-string-in-an-array/maximum-value-of-a-string-in-an-array.java
+++ 2589-maximum-value-of-a-string-in-an-array/maximum-value-of-a-string-in-an-array.java
@@ -1,14 +1,30 @@
 class Solution {
 
     public int maximumValue(String[] strs) {
-        int m = 0;
-        for (int i = 0; i < strs.length; i++) {
-            try {
-                m = Math.max(m, Integer.parseInt(strs[i]));
-            } catch (Exception e) {
-                m = Math.max(m, strs[i].length());
+        // int m = 0;
+        // for (int i = 0; i < strs.length; i++) {
+        //     try {
+        //         m = Math.max(m, Integer.parseInt(strs[i]));
+        //     } catch (Exception e) {
+        //         m = Math.max(m, strs[i].length());
+        //     }
+        // }
+        // return m;
+
+        int max = Integer.MIN_VALUE;
+        for (String s : strs) {
+            int num = 0;
+            for (int j = 0; j < s.length(); j++) {
+                if (s.charAt(j) >= 'a' && s.charAt(j) <= 'z') {
+                    num = s.length();
+                    break;
+                } else {
+                    num = num * 10;
+                    num += (int) (s.charAt(j) - '0');
+                }
             }
+            max = Math.max(max, num);
         }
-        return m;
+        return max;
     }
 }
