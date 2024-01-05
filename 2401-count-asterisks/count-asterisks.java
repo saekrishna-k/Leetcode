f501d8a669ae6cab2e8f1f1a034be31f150a42a8
krish-na-7 <36792759+krish-na-7@users.noreply.github.com>
2024-01-05 18:53:30 +0530

    Time: 2 ms (75.24%) | Memory: 41.7 MB (8.75%) - LeetSync

--- 2401-count-asterisks/count-asterisks.java
+++ 2401-count-asterisks/count-asterisks.java
@@ -1,14 +1,24 @@
 class Solution {
 
     public int countAsterisks(String s) {
-        int count = 0, r = 0, n = s.length();
-        boolean flag;
+        // int count = 0, r = 0, n = s.length();
+        // for (int i = 0; i < n; i++) {
+        //     if (s.charAt(i) == '|') {
+        //         count++;
+        //     }
+        //     if (count % 2 == 0 && s.charAt(i) == '*') {
+        //         r++;
+        //     }
+        // }
+        // return r;
+
+        int r = 0, n = s.length();
+        boolean flag = false;
         for (int i = 0; i < n; i++) {
-            if (s.charAt(i) == '|') {
-                count++;
-            }
-            if (count % 2 == 0 && s.charAt(i) == '*') {
+            if (!flag && s.charAt(i) == '*') {
                 r++;
+            } else if (s.charAt(i) == '|') {
+                flag = !flag;
             }
         }
         return r;
