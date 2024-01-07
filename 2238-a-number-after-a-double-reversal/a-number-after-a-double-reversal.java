4c8d99561d769709086601e2fea9ded07ad93f48
krish-na-7 <36792759+krish-na-7@users.noreply.github.com>
2024-01-07 08:52:40 +0530

    Time: 0 ms (100.00%) | Memory: 40.3 MB (14.31%) - LeetSync

---
+++ 2238-a-number-after-a-double-reversal/a-number-after-a-double-reversal.java
@@ -0,0 +1,24 @@
+class Solution {
+
+    public boolean isSameAfterReversals(int num) {
+        int n = num, r = 0, r1 = 0, i = 0;
+        if (num < 10 && num > -1) {
+            return true;
+        }
+        while (n > 0) {
+            i = n % 10;
+            r = r * 10 + i;
+            n /= 10;
+        }
+        while (r > 0) {
+            i = r % 10;
+            r1 = r1 * 10 + i;
+            r /= 10;
+        }
+        if (num == r1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
