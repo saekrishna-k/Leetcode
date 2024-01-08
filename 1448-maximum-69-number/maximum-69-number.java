e562303d283aa542de9d3cab193fabfb324f2f0f
krish-na-7 <36792759+krish-na-7@users.noreply.github.com>
2024-01-08 09:26:41 +0530

    Time: 9 ms (5.87%) | Memory: 41.5 MB (5.59%) - LeetSync

--- 1448-maximum-69-number/maximum-69-number.java
+++ 1448-maximum-69-number/maximum-69-number.java
@@ -1,26 +1,25 @@
 class Solution {
 
     public int maximum69Number(int num) {
-        // String s = String.valueOf(num);
-        // String s1 = "";
-        // int i = 0;
-        // boolean flag = false;
-        // for (i = 0; i < s.length(); i++) {
-        //     if (flag == false && s.charAt(i) == '6') {
-        //         s1 = s1 + "9";
-        //         flag = !flag;
-        //     } else {
-        //         s1 = s1 + s.charAt(i);
-        //     }
-        // }
-        // return Integer.parseInt(s1);
-
-        String numStr = String.valueOf(num);
-        int first6Idx = numStr.indexOf("6");
-        if (first6Idx == -1) {
-            return num;
+        String s = String.valueOf(num);
+        String s1 = "";
+        int i = 0, l = s.length();
+        boolean flag = false;
+        for (i = 0; i < l; i++) {
+            if (flag == false && s.charAt(i) == '6') {
+                s1 = s1 + "9";
+                flag = !flag;
+            } else {
+                s1 = s1 + s.charAt(i);
+            }
         }
-        return Integer.valueOf(numStr.substring(0, first6Idx) + 9 + numStr.substring(first6Idx + 1));
+        return Integer.parseInt(s1);
+        // String numStr = String.valueOf(num);
+        // int first6Idx = numStr.indexOf("6");
+        // if (first6Idx == -1) {
+        //     return num;
+        // }
+        // return Integer.valueOf(numStr.substring(0, first6Idx) + 9 + numStr.substring(first6Idx + 1));
 
         // String str = Integer.toString(num);
         // str = str.replaceFirst("6", "9");
