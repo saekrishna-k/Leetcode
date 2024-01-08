cc7a77e6bb0e0a7db831fe23e16b5761f34fba05
krish-na-7 <36792759+krish-na-7@users.noreply.github.com>
2024-01-08 09:28:32 +0530

    Time: 0 ms (100.00%) | Memory: 40.5 MB (16.90%) - LeetSync

--- 1448-maximum-69-number/maximum-69-number.java
+++ 1448-maximum-69-number/maximum-69-number.java
@@ -1,20 +1,20 @@
 class Solution {
 
     public int maximum69Number(int num) {
-        String s = String.valueOf(num);
-        String s1 = "";
-        int i = 0, l = s.length();
-        boolean flag = false;
-        for (i = 0; i < l; i++) {
-            if (flag == false && s.charAt(i) == '6') {
-                s1 = s1 + "9";
-                flag = !flag;
-            } else {
-                s1 = s1 + s.charAt(i);
-            }
-        }
-        return Integer.parseInt(s1);
-        
+        // String s = String.valueOf(num);
+        // String s1 = "";
+        // int i = 0, l = s.length();
+        // boolean flag = false;
+        // for (i = 0; i < l; i++) {
+        //     if (flag == false && s.charAt(i) == '6') {
+        //         s1 = s1 + "9";
+        //         flag = !flag;
+        //     } else {
+        //         s1 = s1 + s.charAt(i);
+        //     }
+        // }
+        // return Integer.parseInt(s1);
+
         // String numStr = String.valueOf(num);
         // int first6Idx = numStr.indexOf("6");
         // if (first6Idx == -1) {
@@ -25,5 +25,19 @@ class Solution {
         // String str = Integer.toString(num);
         // str = str.replaceFirst("6", "9");
         // return Integer.parseInt(str);
+
+        String temp = Integer.toString(num);
+        char ch[] = temp.toCharArray();
+        int i = 0;
+        for (char c : ch) {
+            if (c == '6') {
+                ch[i] = '9';
+                break;
+            }
+
+            i++;
+        }
+        String ans = new String(ch);
+        return Integer.valueOf(ans);
     }
 }
