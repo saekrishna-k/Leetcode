4ce77438bfeef47ac8e18eae45a6bb39310879f4
krish-na-7 <36792759+krish-na-7@users.noreply.github.com>
2024-01-16 08:08:49 +0530

    Time: 0 ms (100.00%) | Memory: 40.4 MB (21.87%) - LeetSync

--- 2630-alternating-digit-sum/alternating-digit-sum.java
+++ 2630-alternating-digit-sum/alternating-digit-sum.java
@@ -1,15 +1,22 @@
 class Solution {
 
     public int alternateDigitSum(int n) {
-        int sum = 0, i = 0;
-        String str = String.valueOf(n);
-        for (int j = 0; j < str.length(); j++) {
-            i = Character.getNumericValue(str.charAt(j));
-            if (j % 2 != 0) {
-                sum -= i;
-            } else {
-                sum += i;
-            }
+        // int sum = 0, i = 0;
+        // String str = String.valueOf(n);
+        // for (int j = 0; j < str.length(); j++) {
+        //     i = Character.getNumericValue(str.charAt(j));
+        //     if (j % 2 != 0) {
+        //         sum -= i;
+        //     } else {
+        //         sum += i;
+        //     }
+        // }
+        // return sum;
+
+        int sum = 0;
+        while (n > 0) {
+            sum = n % 10 - sum;
+            n /= 10;
         }
         return sum;
     }
