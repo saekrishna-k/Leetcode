c7d53d298f5ba69d80284bc54f30df03c40e9300
krish-na-7 <36792759+krish-na-7@users.noreply.github.com>
2024-01-19 08:38:48 +0530

    Time: 1 ms (89.81%) | Memory: 40.3 MB (50.41%) - LeetSync

---
+++ 2650-split-with-minimum-sum/split-with-minimum-sum.java
@@ -0,0 +1,21 @@
+class Solution {
+
+    public int splitNum(int num) {
+        int[] n = new int[String.valueOf(num).length()];
+        int a = num, i = 0, num1 = 0, num2 = 0;
+        while (a > 0) {
+            int c = a % 10;
+            n[i] = c;
+            a /= 10;
+            i++;
+        }
+        Arrays.sort(n);
+        for (int j = 0; j < n.length; j = j + 2) {
+            num1 = num1 * 10 + n[j];
+        }
+        for (int j = 1; j < n.length; j = j + 2) {
+            num2 = num2 * 10 + n[j];
+        }
+        return num1 + num2;
+    }
+}
