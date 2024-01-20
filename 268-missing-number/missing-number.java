08c4476c068d738baacc1e1f9b218ff8c75effa1
krish-na-7 <36792759+krish-na-7@users.noreply.github.com>
2024-01-20 09:13:57 +0530

    Time: 6 ms (17.41%) | Memory: 45.2 MB (37.24%) - LeetSync

--- 268-missing-number/missing-number.java
+++ 268-missing-number/missing-number.java
@@ -6,9 +6,9 @@ class Solution {
         if (nums[0] != 0) {
             return 0;
         }
-        for (int i = 0; i < n - 1; i++) {
-            if ((nums[i] + 1) != nums[i + 1]) {
-                return nums[i] + 1;
+        for (int i = 0; i < n; i++) {
+            if ((nums[i]) != i) {
+                return i;
             }
         }
         return n;
