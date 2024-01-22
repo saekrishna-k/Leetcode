1298ab4a55fe959314f0c4fc467da8b228dc3697
krish-na-7 <36792759+krish-na-7@users.noreply.github.com>
2024-01-22 08:59:20 +0530

    Time: 2 ms (97.74%) | Memory: 45.8 MB (17.33%) - LeetSync

--- 645-set-mismatch/set-mismatch.java
+++ 645-set-mismatch/set-mismatch.java
@@ -3,8 +3,8 @@ class Solution {
     public int[] findErrorNums(int[] nums) {
         int duplicate = 0, missing = 0, n = nums.length;
         int[] a = new int[n + 1];
-        for (int i = 0; i < nums.length; i++) {
-            a[nums[i]]++;
+        for (int i : nums) {
+            a[i]++;
         }
         for (int i = 1; i < a.length; i++) {
             if (a[i] == 2) {
