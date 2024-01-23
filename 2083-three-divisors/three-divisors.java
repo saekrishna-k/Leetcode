b2136cfa33a984f345c57df44d44bf837939aece
krish-na-7 <36792759+krish-na-7@users.noreply.github.com>
2024-01-23 08:36:34 +0530

    Time: 0 ms (100.00%) | Memory: 40.7 MB (15.04%) - LeetSync

--- 2083-three-divisors/three-divisors.java
+++ 2083-three-divisors/three-divisors.java
@@ -1,13 +1,13 @@
 class Solution {
 
     public boolean isThree(int n) {
-        int count = 0;
-        for (int i = 1; i < n; i++) {
+        int count = 2;
+        for (int i = 2; i * i <= n; i++) {
             if (n % i == 0) {
-                count++;
+                if (i * i == n) count++; else count += 2;
             }
         }
-        if (++count == 3) {
+        if (count == 3) {
             return true;
         }
         return false;
