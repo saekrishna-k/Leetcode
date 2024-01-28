1eb7e76b32f9c79faccd02a839adde318f5c6126
krish-na-7 <36792759+krish-na-7@users.noreply.github.com>
2024-01-28 18:34:34 +0530

    Time: 0 ms (100.00%) | Memory: 40.8 MB (50.57%) - LeetSync

--- 492-construct-the-rectangle/construct-the-rectangle.java
+++ 492-construct-the-rectangle/construct-the-rectangle.java
@@ -1,10 +1,19 @@
 class Solution {
 
     public int[] constructRectangle(int area) {
-        int m = (int) Math.sqrt(area);
-        while (area % m != 0) {
-            m--;
+        // int m = (int) Math.sqrt(area);
+        // while (area % m != 0) {
+        //     m--;
+        // }
+        // return new int[] { area / m, m };
+
+        int l = (int) Math.sqrt(area);
+        for (int i = l; i >= 1; i--) {
+            if (area % i == 0) {
+                int w = area / i;
+                return new int[] { w, i };
+            }
         }
-        return new int[] { area / m, m };
+        return new int[] {};
     }
 }
