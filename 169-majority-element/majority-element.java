c4aeed4601d72a27b63561b038926335c7f8ebbb
krish-na-7 <36792759+krish-na-7@users.noreply.github.com>
2024-02-12 07:34:19 +0530

    Time: 1 ms (99.82%) | Memory: 50.8 MB (21.94%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -4,17 +4,34 @@ class Solution {
         // Arrays.sort(nums);
         // return nums[nums.length/2];
 
-        int n = nums.length;
-        int x = n / 2;
-        Map<Integer, Integer> m = new HashMap<>();
-        for (int i = 0; i < n; i++) {
-            m.put(nums[i], m.getOrDefault(nums[i], 0) + 1);
-        }
-        for (Map.Entry<Integer, Integer> e : m.entrySet()) {
-            if (e.getValue() > x) {
-                return e.getKey();
+        // int n = nums.length;
+        // int x = n / 2;
+        // Map<Integer, Integer> m = new HashMap<>();
+        // for (int i = 0; i < n; i++) {
+        //     m.put(nums[i], m.getOrDefault(nums[i], 0) + 1);
+        // }
+        // for (Map.Entry<Integer, Integer> e : m.entrySet()) {
+        //     if (e.getValue() > x) {
+        //         return e.getKey();
+        //     }
+        // }
+        // return 0;
+
+        int count = 0;
+        int candidate = 0;
+
+        for (int num : nums) {
+            if (count == 0) {
+                candidate = num;
+            }
+
+            if (num == candidate) {
+                count++;
+            } else {
+                count--;
             }
         }
-        return 0;
+
+        return candidate;
     }
 }
