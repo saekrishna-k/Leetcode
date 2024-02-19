d1b45d57df9b42f82405e9a804437cb249e24f6e
krish-na-7 <36792759+krish-na-7@users.noreply.github.com>
2024-02-19 08:24:34 +0530

    Time: 0 ms (100.00%) | Memory: 40.7 MB (71.58%) - LeetSync

--- 231-power-of-two/power-of-two.java
+++ 231-power-of-two/power-of-two.java
@@ -1,4 +1,5 @@
 class Solution {
+
     public boolean isPowerOfTwo(int n) {
         // return n > 0 && n != 0 && (n & (n-1)) == 0;
 
@@ -18,14 +19,12 @@ class Solution {
         // }
         // return true;
 
-
         // if(n == 0) return false;
         // while(n%2 == 0) n/=2;
         // return n == 1;
 
-        if(n==0) return false;
-        return ((n==1) || (n%2==0 && isPowerOfTwo(n/2)));
-
+        if (n == 0) return false;
+        return ((n == 1) || (n % 2 == 0 && isPowerOfTwo(n / 2)));
         //  if(n <= 0) return false;
         //  return Math.ceil(Math.log(n)) == Math.floor(Math.log(n));
     }
