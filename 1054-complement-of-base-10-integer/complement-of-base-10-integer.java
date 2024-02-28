190226181c5b21b6e5cfc0d6512cc8d3e67ea5fd
krish-na-7 <36792759+krish-na-7@users.noreply.github.com>
2024-02-28 08:32:59 +0530

    Time: 0 ms (100.00%) | Memory: 40.3 MB (69.99%) - LeetSync

--- 1054-complement-of-base-10-integer/complement-of-base-10-integer.java
+++ 1054-complement-of-base-10-integer/complement-of-base-10-integer.java
@@ -1,18 +1,21 @@
 class Solution {
 
     public int bitwiseComplement(int n) {
-        if (n == 0) return 1;
-        int numberOfBits = getNumberOfBits(n);
-        int mask = (1 << numberOfBits) - 1;
-        return n ^ mask;
-    }
+        // if(n == 0) return 1;
+        // int numberOfBits = getNumberOfBits(n);
+        // int mask = (1 << numberOfBits) - 1;
+        // return n ^ mask;
 
-    public static int getNumberOfBits(int n) {
-        int count = 0;
-        while (n != 0) {
-            n = n >> 1;
-            count++;
-        }
-        return count;
+        int mask = 1;
+        while (mask < n) mask = (mask << 1) + 1;
+        return mask ^ n;
     }
+    // public static int getNumberOfBits(int n) {
+    //     int count = 0;
+    //     while (n != 0) {
+    //         n = n >> 1;
+    //         count++;
+    //     }
+    //     return count;
+    // }
 }
