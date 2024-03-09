b701dcbc700be3f36347bfe5f23063511f17d276
krish-na-7 <36792759+krish-na-7@users.noreply.github.com>
2024-03-09 09:58:12 +0530

    Time: 0 ms (100.00%) | Memory: 62.2 MB (88.00%) - LeetSync

--- 2634-minimum-common-value/minimum-common-value.java
+++ 2634-minimum-common-value/minimum-common-value.java
@@ -12,13 +12,18 @@ class Solution {
         // }
         // return -1;
 
-        
         int i = 0, j = 0, l1 = nums1.length, l2 = nums2.length;
         // For faster solution
         if (nums1[l1 - 1] < nums2[0] || nums2[l2 - 1] < nums1[0]) return -1;
         // Two pointer approach
         while (i < l1 && j < l2) {
-            if (nums1[i] == nums2[j]) return nums1[i]; else if (nums1[i] > nums2[j]) j++; else i++;
+            if (nums1[i] == nums2[j]) {
+                return nums1[i];
+            } else if (nums1[i] > nums2[j]) {
+                j++;
+            } else {
+                i++;
+            }
         }
         return -1;
     }
