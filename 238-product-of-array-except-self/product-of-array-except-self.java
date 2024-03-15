fda417fed4ee4c20ee7e12ff6ce2bd93cf713664
krish-na-7 <36792759+krish-na-7@users.noreply.github.com>
2024-03-15 08:42:31 +0530

    Time: 2 ms (73.94%) | Memory: 53.2 MB (58.69%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.java
+++ 238-product-of-array-except-self/product-of-array-except-self.java
@@ -17,7 +17,7 @@ class Solution {
                 result[i] = 0;
             } else if (nums[i] == 0) {
                 result[i] = product;
-            } else if (count == 1 && count != l) {
+            } else if (count == 1) {
                 result[i] = 0;
             } else {
                 result[i] = product / nums[i];
