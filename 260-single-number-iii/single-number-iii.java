b104bd5badcc0b6d61acae6b58773d73e43df652
krish-na-7 <36792759+krish-na-7@users.noreply.github.com>
2024-03-17 07:48:41 +0530

    Time: 1 ms (99.96%) | Memory: 45.7 MB (27.26%) - LeetSync

---
+++ 260-single-number-iii/single-number-iii.java
@@ -0,0 +1,27 @@
+class Solution {
+
+    public int[] singleNumber(int[] nums) {
+        int result[] = new int[2];
+        int xor = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            xor ^= nums[i];
+        }
+
+        // int mask = xor & (xor - 1); // Get the rightmost set bit
+        int mask = xor & (-xor); // Get the rightmost set bit
+        int num1 = 0;
+        int num2 = 0;
+
+        for (int num : nums) {
+            if ((num & mask) > 0) {
+                num1 ^= num;
+            } else {
+                num2 ^= num;
+            }
+        }
+
+        result[0] = num1;
+        result[1] = num2;
+        return result;
+    }
+}
