7a4b66f8008d8154b97999ffbf5ceaf0fc364bf4
krish-na-7 <36792759+krish-na-7@users.noreply.github.com>
2024-03-22 08:11:41 +0530

    Time: 4 ms (93.79%) | Memory: 43.4 MB (58.38%) - LeetSync

--- 2116-count-number-of-pairs-with-absolute-difference-k/count-number-of-pairs-with-absolute-difference-k.java
+++ 2116-count-number-of-pairs-with-absolute-difference-k/count-number-of-pairs-with-absolute-difference-k.java
@@ -4,7 +4,8 @@ class Solution {
         int count = 0, l = nums.length;
         for (int i = 0; i < l; i++) {
             for (int j = i + 1; j < l; j++) {
-                if (Math.abs(nums[i] - nums[j]) == k) {
+                // if (Math.abs(nums[i] - nums[j]) == k) {
+                if (nums[i] - nums[j] == k || nums[j] - nums[i] == k) {
                     count++;
                 }
             }
