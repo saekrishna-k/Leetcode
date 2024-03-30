2897e571c82b17fcf56d9995621005f0f59796a2
saekrishna-k <36792759+saekrishna-k@users.noreply.github.com>
2024-03-30 08:16:07 +0530

    Time: 5 ms (100.00%) | Memory: 44.7 MB (74.55%) - LeetSync

--- 2486-most-frequent-even-element/most-frequent-even-element.java
+++ 2486-most-frequent-even-element/most-frequent-even-element.java
@@ -14,21 +14,37 @@ class Solution {
         // }
         // return result != -1 ? result : -1;
 
-        int maxValue = Integer.MIN_VALUE;
+        // int maxValue = Integer.MIN_VALUE;
+        // for (int num : nums) {
+        //     maxValue = Math.max(num, maxValue);
+        // }
+        // int[] countNums = new int[maxValue + 1];
+        // int result = -1, max = 0;
+        // for (int i : nums) {
+        //     countNums[i]++;
+        // }
+        // for (int i = 0; i < maxValue + 1; i++) {
+        //     if (i % 2 == 0 && max < countNums[i]) {
+        //         result = i;
+        //         max = countNums[i];
+        //     }
+        // }
+        // return result != -1 ? result : -1;
+
+        int max = Integer.MIN_VALUE;
         for (int num : nums) {
-            maxValue = Math.max(num, maxValue);
+            max = Math.max(num, max);
         }
-        int[] countNums = new int[maxValue + 1];
-        int result = -1, max = 0;
-        for (int i : nums) {
-            countNums[i]++;
-        }
-        for (int i = 0; i < maxValue + 1; i++) {
-            if (i % 2 == 0 && max < countNums[i]) {
-                result = i;
-                max = countNums[i];
+        int[] counts = new int[max / 2 + 1];
+        int res = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] % 2 == 1) continue;
+            counts[nums[i] / 2]++;
+            if (counts[nums[i] / 2] > counts[res] || (counts[nums[i] / 2] == counts[res] && nums[i] / 2 < res)) {
+                res = nums[i] / 2;
             }
         }
-        return result != -1 ? result : -1;
+        if (counts[res] == 0) return -1;
+        return res * 2;
     }
 }
