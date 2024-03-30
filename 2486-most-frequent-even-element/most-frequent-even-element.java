47971f01e24d12afd7003669a0cf5b05150fd2cb
saekrishna-k <36792759+saekrishna-k@users.noreply.github.com>
2024-03-30 08:12:10 +0530

    Time: 23 ms (40.53%) | Memory: 45.1 MB (22.89%) - LeetSync

--- 2486-most-frequent-even-element/most-frequent-even-element.java
+++ 2486-most-frequent-even-element/most-frequent-even-element.java
@@ -1,12 +1,29 @@
 class Solution {
 
     public int mostFrequentEven(int[] nums) {
-        int[] countNums = new int[100000];
+        // int[] countNums = new int[100000];
+        // int result = -1, max = 0;
+        // for (int i : nums) {
+        //     countNums[i]++;
+        // }
+        // for (int i = 0; i < 100000; i++) {
+        //     if (i % 2 == 0 && max < countNums[i]) {
+        //         result = i;
+        //         max = countNums[i];
+        //     }
+        // }
+        // return result != -1 ? result : -1;
+
+        int maxValue = Integer.MIN_VALUE;
+        for (int num : nums) {
+            maxValue = Math.max(num, maxValue);
+        }
+        int[] countNums = new int[maxValue + 1];
         int result = -1, max = 0;
         for (int i : nums) {
             countNums[i]++;
         }
-        for (int i = 0; i < 100000; i++) {
+        for (int i = 0; i < maxValue + 1; i++) {
             if (i % 2 == 0 && max < countNums[i]) {
                 result = i;
                 max = countNums[i];
