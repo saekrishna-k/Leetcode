f91db16b3e4db4301cfc8e8158a3ce0df50a807b
saekrishna-k <36792759+saekrishna-k@users.noreply.github.com>
2024-04-11 11:18:43 +0530

    Time: 1 ms (67.89%) | Memory: 41.3 MB (93.16%) - LeetSync

--- 2177-check-whether-two-strings-are-almost-equivalent/check-whether-two-strings-are-almost-equivalent.java
+++ 2177-check-whether-two-strings-are-almost-equivalent/check-whether-two-strings-are-almost-equivalent.java
@@ -5,8 +5,6 @@ class Solution {
         int l1 = word1.length(), l2 = word2.length();
         for (int i = 0; i < l1; i++) {
             frequency[word1.charAt(i) - 'a']++;
-        }
-        for (int i = 0; i < l2; i++) {
             frequency[word2.charAt(i) - 'a']--;
         }
         for (int i : frequency) {
