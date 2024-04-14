b4d7aaa81ab4b638f05b9071824de6fc49f765af
saekrishna-k <36792759+saekrishna-k@users.noreply.github.com>
2024-04-14 09:22:33 +0530

    Time: 0 ms (100.00%) | Memory: 41.2 MB (82.17%) - LeetSync

--- 1915-check-if-one-string-swap-can-make-strings-equal/check-if-one-string-swap-can-make-strings-equal.java
+++ 1915-check-if-one-string-swap-can-make-strings-equal/check-if-one-string-swap-can-make-strings-equal.java
@@ -1,20 +1,48 @@
 class Solution {
 
     public boolean areAlmostEqual(String s1, String s2) {
+        // if (s1.equals(s2)) return true;
+        // int[] frequency = new int[26];
+        // int count = 0;
+        // for (int i = 0; i < s1.length(); i++) {
+        //     frequency[s1.charAt(i) - 'a']++;
+        //     frequency[s2.charAt(i) - 'a']--;
+        //     if (s1.charAt(i) != s2.charAt(i)) count++;
+        //     if (count > 2) return false;
+        // }
+        // for (int i : frequency) {
+        //     if (i > 0) {
+        //         return false;
+        //     }
+        // }
+        // return true;
+
+        if (!s1.isEmpty() && s2.isEmpty()) return false;
+
+        if (s1.isEmpty() && !s2.isEmpty()) return false;
+
         if (s1.equals(s2)) return true;
-        int[] frequency = new int[26];
-        int count = 0;
+
+        int differences = 0;
+        int index1 = -1;
+        int index2 = -1;
+
         for (int i = 0; i < s1.length(); i++) {
-            frequency[s1.charAt(i) - 'a']++;
-            frequency[s2.charAt(i) - 'a']--;
-            if (s1.charAt(i) != s2.charAt(i)) count++;
-            if (count > 2) return false;
-        }
-        for (int i : frequency) {
-            if (i > 0) {
-                return false;
+            if (s1.charAt(i) != s2.charAt(i)) {
+                differences++;
+                if (index1 == -1) {
+                    index1 = i;
+                } else {
+                    index2 = i;
+                }
             }
         }
-        return true;
+
+        return differences == 0 
+            || (
+                differences == 2 
+                && s1.charAt(index1) == s2.charAt(index2) 
+                && s2.charAt(index1) == s1.charAt(index2)
+            );
     }
 }
