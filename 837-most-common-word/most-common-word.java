e2e8ced268dfda3da3b9d1b2768670cb1c2fbab0
saekrishna-k <36792759+saekrishna-k@users.noreply.github.com>
2024-04-17 07:49:47 +0530

    Time: 13 ms (72.51%) | Memory: 42.7 MB (75.31%) - LeetSync

--- 837-most-common-word/most-common-word.java
+++ 837-most-common-word/most-common-word.java
@@ -1,10 +1,23 @@
 class Solution {
 
     public String mostCommonWord(String paragraph, String[] banned) {
-        Set<String> bannedString = new HashSet<>(Arrays.asList(banned));
-        Map<String, Integer> count = new HashMap<>();
-        String[] words = paragraph.replaceAll("\\W+", " ").toLowerCase().split("\\s+");
-        for (String w : words) if (!bannedString.contains(w)) count.put(w, count.getOrDefault(w, 0) + 1);
-        return Collections.max(count.entrySet(), Map.Entry.comparingByValue()).getKey();
+        // Set<String> bannedString = new HashSet<>(Arrays.asList(banned));
+        // Map<String, Integer> count = new HashMap<>();
+        // String[] words = paragraph.replaceAll("\\W+", " ").toLowerCase().split("\\s+");
+        // for (String w : words) if (!bannedString.contains(w)) count.put(w, count.getOrDefault(w, 0) + 1);
+        // return Collections.max(count.entrySet(), Map.Entry.comparingByValue()).getKey();
+
+        String[] words = paragraph.toLowerCase().split("[ !?',;.]+");
+        HashMap<String, Integer> map = new HashMap<>();
+
+        for (String element : words) map.put(element, map.getOrDefault(element, 0) + 1);
+
+        for (String word : banned) if (map.containsKey(word)) map.remove(word);
+
+        String result = null;
+        for (String word : map.keySet()) {
+            if (result == null || map.get(word) > map.get(result)) result = word;
+        }
+        return result;
     }
 }
