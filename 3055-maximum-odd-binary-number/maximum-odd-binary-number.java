c6f2e6559f387a6b5fcdda276c4f1d9cf20238b9
saekrishna-k <36792759+saekrishna-k@users.noreply.github.com>
2024-04-26 13:14:21 +0530

    Time: 2 ms (74.63%) | Memory: 42.6 MB (97.43%) - LeetSync

--- 3055-maximum-odd-binary-number/maximum-odd-binary-number.java
+++ 3055-maximum-odd-binary-number/maximum-odd-binary-number.java
@@ -1,15 +1,31 @@
 class Solution {
 
     public String maximumOddBinaryNumber(String s) {
-        int onesCount = 0, zerosCount = 0;
-        for (char c : s.toCharArray()) {
-            if (c == '0') zerosCount++; else if (c == '1') onesCount++;
+        // int onesCount = 0, zerosCount = 0;
+        // for (char c : s.toCharArray()) {
+        //     if (c == '0') zerosCount++; else if (c == '1') onesCount++;
+        // }
+        // StringBuilder result = new StringBuilder();
+        // result.append("1".repeat(onesCount - 1));
+        // result.append("0".repeat(zerosCount));
+        // result.append("1");
+
+        // return result.toString();
+
+        int onesCount = 0, zerosCount = 0, l = s.length();
+        for (int i = 0; i < l; i++) {
+            if (s.charAt(i) == '0') zerosCount++; else if (s.charAt(i) == '1') onesCount++;
         }
         StringBuilder result = new StringBuilder();
-        result.append("1".repeat(onesCount - 1));
-        result.append("0".repeat(zerosCount));
-        result.append("1");
-
-        return result.toString();
+        for (int i = 0; i < l; i++) {
+            if (onesCount > 1) {
+                result.append("1");
+                onesCount--;
+            } else if (onesCount == 1 && zerosCount > 0) {
+                result.append("0");
+                zerosCount--;
+            }
+        }
+        return result.append("1").toString();
     }
 }
