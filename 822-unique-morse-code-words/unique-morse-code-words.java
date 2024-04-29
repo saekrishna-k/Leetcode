4266978f352b3dfe530885c268a8c98c6ca3d235
saekrishna-k <36792759+saekrishna-k@users.noreply.github.com>
2024-04-29 08:04:05 +0530

    Time: 1 ms (100.00%) | Memory: 41.7 MB (29.93%) - LeetSync

--- 822-unique-morse-code-words/unique-morse-code-words.java
+++ 822-unique-morse-code-words/unique-morse-code-words.java
@@ -31,12 +31,22 @@ class Solution {
         };
         Set<String> result = new HashSet<String>();
         for (String word : words) {
-            StringBuilder morseCodedString = new StringBuilder();
-            for (int j = 0; j < word.length(); j++) {
-                morseCodedString.append(codes[word.charAt(j) - 'a']);
-            }
-            result.add(morseCodedString.toString());
+            // StringBuilder morseCodedString = new StringBuilder();
+            // for (int j = 0; j < word.length(); j++) {
+            //     morseCodedString.append(codes[word.charAt(j) - 'a']);
+            // }
+            // result.add(morseCodedString.toString());
+
+            result.add(convertTOMorseCode(word, codes));
         }
         return result.size();
+    }
+
+    private static String convertTOMorseCode(String word, String[] codes) {
+        StringBuilder morseCodedString = new StringBuilder();
+        for (int j = 0; j < word.length(); j++) {
+            morseCodedString.append(codes[word.charAt(j) - 'a']);
+        }
+        return morseCodedString.toString();
     }
 }
