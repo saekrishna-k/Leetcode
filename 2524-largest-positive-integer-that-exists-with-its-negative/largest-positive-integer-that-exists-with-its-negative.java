b56bf0c4fcf18bd6099b3874f026cade930f5776
saekrishna-k <36792759+saekrishna-k@users.noreply.github.com>
2024-05-02 07:05:38 +0530

    Time: 2 ms (96.21%) | Memory: 44.6 MB (72.45%) - LeetSync

--- 2524-largest-positive-integer-that-exists-with-its-negative/largest-positive-integer-that-exists-with-its-negative.java
+++ 2524-largest-positive-integer-that-exists-with-its-negative/largest-positive-integer-that-exists-with-its-negative.java
@@ -1,26 +1,39 @@
 class Solution {
 
     public int findMaxK(int[] nums) {
-        Map<Integer, Integer> map = new HashMap<>();
-        int max = 0;
+        // Map<Integer, Integer> map = new HashMap<>();
+        // int max = 0;
+        // for (int i : nums) {
+        //     int j;
+        //     if (i > 0) {
+        //         j = -i;
+        //     } else {
+        //         j = Math.abs(i);
+        //     }
+        //     if (map.containsKey(j)) {
+        //         if (j < 0) {
+        //             max = max < i ? i : max;
+        //         } else {
+        //             max = max < j ? j : max;
+        //         }
+        //     }
+        //     if (!map.containsKey(i)) {
+        //         map.put(i, 0);
+        //     }
+        // }
+        // return max == 0 ? -1 : max;
+
+        int[] sum = new int[1001];
+        int max = -1;
         for (int i : nums) {
-            int j;
-            if (i > 0) {
-                j = -i;
-            } else {
-                j = Math.abs(i);
+            int index = i > 0 ? i : -i;
+            if (sum[index] != i) {
+                sum[index] += i;
             }
-            if (map.containsKey(j)) {
-                if (j < 0) {
-                    max = max < i ? i : max;
-                } else {
-                    max = max < j ? j : max;
-                }
-            }
-            if (!map.containsKey(i)) {
-                map.put(i, 0);
+            if (sum[index] == 0) {
+                max = max > index ? max : index;
             }
         }
-        return max == 0 ? -1 : max;
+        return max;
     }
 }
