3bf694187e8e1d7fe13421291c7824d6b81a6b24
saekrishna-k <36792759+saekrishna-k@users.noreply.github.com>
2024-05-03 07:15:21 +0530

    Time: 1 ms (99.83%) | Memory: 42.3 MB (15.23%) - LeetSync

--- 3312-number-of-changing-keys/number-of-changing-keys.java
+++ 3312-number-of-changing-keys/number-of-changing-keys.java
@@ -2,12 +2,12 @@ class Solution {
 
     public int countKeyChanges(String s) {
         String str = s.toLowerCase();
-        int c = 0, l = str.length();
+        int count = 0, l = str.length();
         for (int i = 0; i < l - 1; i++) {
             if (str.charAt(i) != str.charAt(i + 1)) {
-                c++;
+                count++;
             }
         }
-        return c;
+        return count;
     }
 }
