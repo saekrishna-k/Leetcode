a35544f0671c65239c6aaf931d5c21619552a3b0
saekrishna-k <36792759+saekrishna-k@users.noreply.github.com>
2024-05-06 07:02:06 +0530

    Time: 1 ms (77.50%) | Memory: 41.5 MB (29.83%) - LeetSync

--- 1490-generate-a-string-with-characters-that-have-odd-counts/generate-a-string-with-characters-that-have-odd-counts.java
+++ 1490-generate-a-string-with-characters-that-have-odd-counts/generate-a-string-with-characters-that-have-odd-counts.java
@@ -2,16 +2,22 @@ class Solution {
 
     public String generateTheString(int n) {
         // return "b" + "ab".substring(n % 2, 1 + n % 2).repeat(n - 1);
-        int count = (n % 2 == 0) ? n - 1 : n;
+        // int count = (n % 2 == 0) ? n - 1 : n;
+        // StringBuilder result = new StringBuilder();
+
+        // for (int i = 0; i < count; i++) {
+        //     result.append('a');
+        // }
+
+        // if (n % 2 == 0) {
+        //     result.append('b');
+        // }
+        // return result.toString();
+
         StringBuilder result = new StringBuilder();
-
-        for (int i = 0; i < count; i++) {
-            result.append('a');
-        }
-
-        if (n % 2 == 0) {
-            result.append('b');
-        }
+        int count = n % 2 == 0 ? n-1 : n;
+        result.append("a".repeat(count));
+        if(count != n) result.append('b');
         return result.toString();
     }
 }
