a0a3de7270150f5bed0c614f937a59d996cd2147
saekrishna-k <36792759+saekrishna-k@users.noreply.github.com>
2024-05-09 06:59:17 +0530

    Time: 0 ms (100.00%) | Memory: 42.5 MB (72.61%) - LeetSync

--- 1574-maximum-product-of-two-elements-in-an-array/maximum-product-of-two-elements-in-an-array.java
+++ 1574-maximum-product-of-two-elements-in-an-array/maximum-product-of-two-elements-in-an-array.java
@@ -12,25 +12,24 @@ class Solution {
         // }
         // return max;
 
-        // int max1 = 0;
-        // int max2 = 0;
-        // for (int num : nums) {
-        //     if (num > max1) {
-        //         max2 = max1;
-        //         max1 = num;
-        //     } else if (num > max2) {
-        //         max2 = num;
-        //     }
-        // }
-        // return (max1 - 1) * (max2 - 1);
-
-        int n = nums.length;
-        int currentmax = nums[0];
-        int result = 0;
-        for (int i = 1; i < nums.length; i++) {
-            result = Math.max(result, (nums[i] - 1) * (currentmax - 1));
-            currentmax = Math.max(currentmax, nums[i]);
+        int max1 = 0;
+        int max2 = 0;
+        for (int num : nums) {
+            if (num > max1) {
+                max2 = max1;
+                max1 = num;
+            } else if (num > max2) {
+                max2 = num;
+            }
         }
-        return result;
+        return (max1 - 1) * (max2 - 1);
+        // int n = nums.length;
+        // int currentmax = nums[0];
+        // int result = 0;
+        // for (int i = 1; i < nums.length; i++) {
+        //     result = Math.max(result, (nums[i] - 1) * (currentmax - 1));
+        //     currentmax = Math.max(currentmax, nums[i]);
+        // }
+        // return result;
     }
 }
