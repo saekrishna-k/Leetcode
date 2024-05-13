90cc9a24ccf4a8a009e133a2905d1d7c89ffba43
saekrishna-k <36792759+saekrishna-k@users.noreply.github.com>
2024-05-13 23:21:05 +0530

    Time: 0 ms (100.00%) | Memory: 44.6 MB (72.90%) - LeetSync

--- 1241-decompress-run-length-encoded-list/decompress-run-length-encoded-list.java
+++ 1241-decompress-run-length-encoded-list/decompress-run-length-encoded-list.java
@@ -1,13 +1,25 @@
 class Solution {
 
     public int[] decompressRLElist(int[] nums) {
-        List<Integer> list = new ArrayList<>();
-        for (int i = 0; i < nums.length - 1; i = i + 2) {
-            int index = i + 1;
-            for (int j = 0; j < nums[i]; j++) {
-                list.add(nums[index]);
-            }
+        // List<Integer> list = new ArrayList<>();
+        // for (int i = 0; i < nums.length - 1; i = i + 2) {
+        //     int index = i + 1;
+        //     for (int j = 0; j < nums[i]; j++) {
+        //         list.add(nums[index]);
+        //     }
+        // }
+        // return list.stream().mapToInt(Integer::intValue).toArray();
+
+        int size = 0, l = nums.length;
+        for (int i = 0; i < nums.length; i = i + 2) {
+            size = size + nums[i];
         }
-        return list.stream().mapToInt(Integer::intValue).toArray();
+        int[] result = new int[size];
+        int start = 0;
+        for (int i = 0; i < nums.length; i = i + 2) {
+            Arrays.fill(result, start, start + nums[i], nums[i + 1]);
+            start = start + nums[i];
+        }
+        return result;
     }
 }
