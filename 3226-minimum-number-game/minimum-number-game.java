e779e00d73ca37af3f48ee08ff20a39ad054ceca
saekrishna-k <36792759+saekrishna-k@users.noreply.github.com>
2024-05-14 18:35:29 +0530

    Time: 2 ms (97.23%) | Memory: 44.6 MB (67.18%) - LeetSync

---
+++ 3226-minimum-number-game/minimum-number-game.java
@@ -0,0 +1,26 @@
+class Solution {
+
+    public int[] numberGame(int[] nums) {
+        int l = nums.length;
+        for (int i = 0; i < l - 1; i++) {
+            boolean swapped = false;
+            for (int j = 0; j < l - i - 1; j++) {
+                if (nums[j] > nums[j + 1]) {
+                    int temp = nums[j];
+                    nums[j] = nums[j + 1];
+                    nums[j + 1] = temp;
+                    swapped = !swapped;
+                }
+            }
+        }
+
+        for (int i = 0; i < l; i = i + 2) {
+            if (i + 1 < l) {
+                int temp = nums[i];
+                nums[i] = nums[i + 1];
+                nums[i + 1] = temp;
+            }
+        }
+        return nums;
+    }
+}
