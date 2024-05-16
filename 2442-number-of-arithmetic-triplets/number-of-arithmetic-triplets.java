d24bbc6a57621d2feed8ab9ceb5aee82c4bc0412
saekrishna-k <36792759+saekrishna-k@users.noreply.github.com>
2024-05-16 06:46:47 +0530

    Time: 0 ms (100.00%) | Memory: 41.4 MB (59.25%) - LeetSync

--- 2442-number-of-arithmetic-triplets/number-of-arithmetic-triplets.java
+++ 2442-number-of-arithmetic-triplets/number-of-arithmetic-triplets.java
@@ -1,16 +1,28 @@
 class Solution {
 
     public int arithmeticTriplets(int[] nums, int diff) {
-        int result = 0, l = nums.length;
-        for (int i = 0; i < l; i++) {
-            for (int j = i + 1; j < l; j++) {
-                for (int k = j + 1; k < l; k++) {
-                    if (nums[j] - nums[i] == diff && nums[k] - nums[j] == diff) {
-                        result++;
-                    }
-                }
+        // int result = 0, l = nums.length;
+        // for (int i = 0; i < l; i++) {
+        //     for (int j = i + 1; j < l; j++) {
+        //         for (int k = j + 1; k < l; k++) {
+        //             if (nums[j] - nums[i] == diff && nums[k] - nums[j] == diff) {
+        //                 result++;
+        //             }
+        //         }
+        //     }
+        // }
+        // return result;
+
+        int result = 0;
+        boolean[] count = new boolean[201];
+
+        for (int num : nums) {
+            if (num >= 2 * diff && count[num - diff] && count[num - 2 * diff]) {
+                result++;
             }
+            count[num] = true;
         }
+
         return result;
     }
 }
