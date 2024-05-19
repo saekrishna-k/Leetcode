4a31bce8e88a6a49a3be033c66f87234055b8981
saekrishna-k <36792759+saekrishna-k@users.noreply.github.com>
2024-05-19 20:37:53 +0530

    Time: 4 ms (86.63%) | Memory: 44.4 MB (91.38%) - LeetSync

--- 2639-separate-the-digits-in-an-array/separate-the-digits-in-an-array.java
+++ 2639-separate-the-digits-in-an-array/separate-the-digits-in-an-array.java
@@ -1,18 +1,34 @@
 class Solution {
 
     public int[] separateDigits(int[] nums) {
-        List<Integer> result = new ArrayList<>();
-        for (int i : nums) {
-            List<Integer> temp = new ArrayList<>();
-            int a = i;
-            while (a > 0) {
-                temp.add(a % 10);
-                a /= 10;
-            }
-            for (int j = temp.size() - 1; j >= 0; j--) {
-                result.add(temp.get(j));
+        // List<Integer> result = new ArrayList<>();
+        // for (int i : nums) {
+        //     List<Integer> temp = new ArrayList<>();
+        //     int a = i;
+        //     while (a > 0) {
+        //         temp.add(a % 10);
+        //         a /= 10;
+        //     }
+        //     for (int j = temp.size() - 1; j >= 0; j--) {
+        //         result.add(temp.get(j));
+        //     }
+        // }
+        // return result.stream().mapToInt(Integer::intValue).toArray();
+
+        List<Integer> list = new ArrayList();
+        for (int i = nums.length - 1; i >= 0; i--) {
+            int temp = nums[i];
+            while (temp > 0) {
+                list.add(temp % 10);
+                temp /= 10;
             }
         }
-        return result.stream().mapToInt(Integer::intValue).toArray();
+        int[] result = new int[list.size()];
+        int index = 0;
+        for (int i = list.size() - 1; i >= 0; i--) {
+            result[index] = list.get(i);
+            index++;
+        }
+        return result;
     }
 }
