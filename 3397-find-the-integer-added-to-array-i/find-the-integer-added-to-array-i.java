8225d69d968a99339adc5490006b886030d3e3cd
saekrishna-k <36792759+saekrishna-k@users.noreply.github.com>
2024-05-22 09:38:51 +0530

    Time: 0 ms (100.00%) | Memory: 42.8 MB (88.33%) - LeetSync

--- 3397-find-the-integer-added-to-array-i/find-the-integer-added-to-array-i.java
+++ 3397-find-the-integer-added-to-array-i/find-the-integer-added-to-array-i.java
@@ -1,9 +1,24 @@
 class Solution {
 
     public int addedInteger(int[] nums1, int[] nums2) {
-        Arrays.sort(nums1);
-        Arrays.sort(nums2);
+        // Arrays.sort(nums1);
+        // Arrays.sort(nums2);
 
-        return nums2[0] - nums1[0];
+        // return nums2[0] - nums1[0];
+
+        int min1 = Integer.MAX_VALUE;
+        int min2 = Integer.MAX_VALUE;
+
+        for (int i = 0; i < nums1.length; i++) {
+            if (min1 > nums1[i]) {
+                min1 = nums1[i];
+            }
+
+            if (min2 > nums2[i]) {
+                min2 = nums2[i];
+            }
+        }
+
+        return min2 - min1;
     }
 }
