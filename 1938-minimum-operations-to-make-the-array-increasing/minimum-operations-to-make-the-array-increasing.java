adf78d457470a2058276b4d70931e6477735b1e0
saekrishna-k <36792759+saekrishna-k@users.noreply.github.com>
2024-05-24 23:25:09 +0530

    Time: 2 ms (100.00%) | Memory: 44.7 MB (88.26%) - LeetSync

--- 1938-minimum-operations-to-make-the-array-increasing/minimum-operations-to-make-the-array-increasing.java
+++ 1938-minimum-operations-to-make-the-array-increasing/minimum-operations-to-make-the-array-increasing.java
@@ -1,17 +1,30 @@
 class Solution {
 
     public int minOperations(int[] nums) {
-        int noOfOperations = 0;
-        for (int i = 0; i < nums.length - 1; i++) {
-            if (nums[i] == nums[i + 1]) {
-                nums[i + 1]++;
-                noOfOperations++;
-            } else if (nums[i] > nums[i + 1]) {
-                int diff = nums[i] - nums[i + 1] + 1;
-                nums[i + 1] += diff;
-                noOfOperations += diff;
+        // int noOfOperations = 0;
+        // for (int i = 0; i < nums.length - 1; i++) {
+        //     if (nums[i] == nums[i + 1]) {
+        //         nums[i + 1]++;
+        //         noOfOperations++;
+        //     } else if (nums[i] > nums[i + 1]) {
+        //         int diff = nums[i] - nums[i + 1] + 1;
+        //         nums[i + 1] += diff;
+        //         noOfOperations += diff;
+        //     }
+        // }
+        // return noOfOperations;
+
+        int noOfOperations = 0, max = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] > max) {
+                max = nums[i];
+            } else {
+                max++;
+                noOfOperations += max - nums[i];
             }
         }
+
         return noOfOperations;
     }
 }
