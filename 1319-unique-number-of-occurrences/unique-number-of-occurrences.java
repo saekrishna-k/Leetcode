9f1cd8cd21870b25a860bfd3be460c90605c0d0e
saekrishna-k <36792759+saekrishna-k@users.noreply.github.com>
2024-05-30 07:24:50 +0530

    Time: 0 ms (100.00%) | Memory: 41.5 MB (85.65%) - LeetSync

--- 1319-unique-number-of-occurrences/unique-number-of-occurrences.java
+++ 1319-unique-number-of-occurrences/unique-number-of-occurrences.java
@@ -9,20 +9,35 @@ class Solution {
         // Set<Integer> set = new HashSet<>(map.values());
         // return set.size() == map.size();
 
-        Arrays.sort(arr);
-        HashSet<Integer> set = new HashSet<>();
+        //     Arrays.sort(arr);
+        //     HashSet<Integer> set = new HashSet<>();
+        //     int count = 1;
+        //     for (int i = 1; i < arr.length; i++) {
+        //         if (arr[i] == arr[i - 1]) count++; else {
+        //             if (set.contains(count)) return false;
+        //             set.add(count);
+        //             count = 1;
+        //         }
+        //     }
+        //     if (set.contains(count)) return false;
+        //     return true;
+        // }
 
-        int count = 1;
-        for (int i = 1; i < arr.length; i++) {
-            if (arr[i] == arr[i - 1]) count++; else {
-                if (set.contains(count)) return false;
 
-                set.add(count);
-                count = 1;
-            }
+
+        int[] numberOfOccurrences = new int[2001];
+        boolean[] used = new boolean[1001];
+        for (int i : arr) {
+            numberOfOccurrences[1000 + i]++;
         }
 
-        if (set.contains(count)) return false;
+        for (int i : arr) {
+            int count = numberOfOccurrences[1000 + i];
+            numberOfOccurrences[1000 + i] = 0;
+            if (count > 0) {
+                if (used[count]) return false; else used[count] = true;
+            }
+        }
         return true;
     }
 }
