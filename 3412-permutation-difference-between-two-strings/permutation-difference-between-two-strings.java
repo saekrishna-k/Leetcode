dba87444722286241a6c11f4fe60f06b8951ce7a
saekrishna-k <36792759+saekrishna-k@users.noreply.github.com>
2024-06-01 07:57:16 +0530

    Time: 132 ms (17.71%) | Memory: 42.3 MB (80.00%) - LeetSync

--- 3412-permutation-difference-between-two-strings/permutation-difference-between-two-strings.java
+++ 3412-permutation-difference-between-two-strings/permutation-difference-between-two-strings.java
@@ -1,11 +1,21 @@
 class Solution {
 
     public int findPermutationDifference(String s, String t) {
-        int sum = 0;
-        for (int i = 0; i < s.length(); i++) {
-            sum += Math.abs(i - t.indexOf(s.charAt(i)));
+        // int sum = 0;
+        // for (int i = 0; i < s.length(); i++) {
+        //     sum += Math.abs(i - t.indexOf(s.charAt(i)));
+        // }
+
+        // return sum;
+
+        return iterate(0, s, t);
+    }
+
+    public static int iterate(int n, String s, String t) {
+        if (n == s.length()) {
+            return 0;
         }
 
-        return sum;
+        return Math.abs(n - t.indexOf(s.charAt(n))) + iterate(n + 1, s, t);
     }
 }
