5d453318da0db994b16d2d3c9e73793f3c3ea6a8
saekrishna-k <36792759+saekrishna-k@users.noreply.github.com>
2024-06-04 09:10:34 +0530

    Time: 1 ms (100.00%) | Memory: 41.3 MB (90.37%) - LeetSync

--- 409-longest-palindrome/longest-palindrome.java
+++ 409-longest-palindrome/longest-palindrome.java
@@ -10,6 +10,9 @@ class Solution {
 
         for (int count : frequency) {
             result += count / 2 * 2;
+            // if(result % 2 == 0 && count % 2 == 1){
+            //     result += 1;
+            // }
         }
         return result < s.length() ? result + 1 : result;
     }
