6fc8e75dbed4d553a969b2e92afb0169c71c5224
saekrishna-k <36792759+saekrishna-k@users.noreply.github.com>
2024-06-05 23:15:39 +0530

    Time: 4 ms (99.49%) | Memory: 45.1 MB (25.54%) - LeetSync

--- 2836-neither-minimum-nor-maximum/neither-minimum-nor-maximum.java
+++ 2836-neither-minimum-nor-maximum/neither-minimum-nor-maximum.java
@@ -8,7 +8,6 @@ class Solution {
         // return nums[1];
 
         if (nums.length < 3) return -1;
-
         int a = Math.min(nums[0], nums[1]);
         int b = Math.max(nums[0], nums[1]);
         int c = nums[2];
