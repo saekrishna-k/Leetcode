67a3b7b30a4f45ef02d29e29c73eb314a334d514
saekrishna-k <36792759+saekrishna-k@users.noreply.github.com>
2024-06-06 09:19:32 +0530

    Time: 0 ms (100.00%) | Memory: 41.2 MB (66.04%) - LeetSync

--- 1137-height-checker/height-checker.java
+++ 1137-height-checker/height-checker.java
@@ -1,36 +1,35 @@
 class Solution {
 
     public int heightChecker(int[] heights) {
+        // int count = 0;
+        // int[] copy = heights.clone();
+        // Arrays.sort(copy);
+        // for (int i = 0; i < heights.length; i++) {
+        //     if (heights[i] != copy[i]) {
+        //         count++;
+        //     }
+        // }
+        // return count;
+
+        int[] frequency = new int[101];
+        for (int height : heights) {
+            frequency[height]++;
+        }
+
         int count = 0;
-        int[] copy = heights.clone();
-        Arrays.sort(copy);
+        int curHeight = 0;
+
         for (int i = 0; i < heights.length; i++) {
-            if (heights[i] != copy[i]) {
+            while (frequency[curHeight] == 0) {
+                curHeight++;
+            }
+
+            if (curHeight != heights[i]) {
                 count++;
             }
+            frequency[curHeight]--;
         }
+
         return count;
-
-
-        // int[] frequency = new int[101];
-        // for (int height : heights) {
-        //     frequency[height]++;
-        // }
-
-        // int result = 0;
-        // int curHeight = 0;
-
-        // for (int i = 0; i < heights.length; i++) {
-        //     while (frequency[curHeight] == 0) {
-        //         curHeight++;
-        //     }
-
-        //     if (curHeight != heights[i]) {
-        //         result++;
-        //     }
-        //     frequency[curHeight]--;
-        // }
-
-        // return result;
     }
 }
