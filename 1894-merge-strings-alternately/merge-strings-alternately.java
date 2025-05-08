a9d30bc97cafc0a77b6749a560d93d054fa88bbd
saekrishna-k <36792759+saekrishna-k@users.noreply.github.com>
2025-05-08 18:20:24 +0530

    Time: 0 ms (100.00%) | Memory: 41.7 MB (71.85%) - LeetSync

--- 1894-merge-strings-alternately/merge-strings-alternately.java
+++ 1894-merge-strings-alternately/merge-strings-alternately.java
@@ -1,37 +1,21 @@
 class Solution {
+
     public String mergeAlternately(String word1, String word2) {
-        // int l1 = word1.length();
-        // int l2 = word2.length();
-        // String r = "";
-        // if(l1<l2){
-        //         for(int i = 0 ; i < l1 ; i++){
-        //         r = r + word1.charAt(i) + word2.charAt(i);
-        //     }
-        //     r = r + word2.substring(l1);
-        // }else if(l1>l2){
-        //         for(int i = 0 ; i < l2 ; i++){
-        //         r = r + word1.charAt(i) + word2.charAt(i);
-        //     }
-        //     r = r + word1.substring(l2);
-        // }else{
-        //     for(int i = 0 ; i < l1 ; i++){
-        //         r = r + word1.charAt(i) + word2.charAt(i);
-        //     }
-        // }
-        // return r;
-                int l1 = word1.length();
+        int l1 = word1.length();
         int l2 = word2.length();
-          StringBuilder result = new StringBuilder();
-        int i = 0;
-        while (i < l1 || i < l2) {
-            if (i < l1) {
-                result.append(word1.charAt(i));
-            }
-            if (i < l2) {
-                result.append(word2.charAt(i));
-            }
-            i++;
+        StringBuilder s1 = new StringBuilder();
+        int diff = l1 < l2 ? l1 : l2;
+
+        for (int i = 0; i < diff; i++) {
+            s1.append(word1.charAt(i));
+            s1.append(word2.charAt(i));
         }
-        return result.toString();
+
+        if (l1 > l2) {
+            s1.append(word1.substring(diff, l1));
+        } else {
+            s1.append(word2.substring(diff, l2));
+        }
+        return s1.toString();
     }
 }
