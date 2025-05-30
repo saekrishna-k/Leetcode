42723b94d820ccd52a3c54acfc90faaefbc19ba7
saekrishna-k <36792759+saekrishna-k@users.noreply.github.com>
2025-05-30 18:36:13 +0530

    Time: 3 ms (22.26%) | Memory: 45.7 MB (96.05%) - LeetSync

--- 283-move-zeroes/move-zeroes.java
+++ 283-move-zeroes/move-zeroes.java
@@ -11,21 +11,53 @@ class Solution {
         //     nums[j++] = 0;
         // }
 
-        int index = -1;
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] == 0) {
-                index = i;
-                break;
+
+
+
+        // int index = -1;
+        // for(int i = 0 ; i < nums.length ; i++){
+        //     if(nums[i] == 0){
+        //         index = i;
+        //         break;
+        //     }
+        // }
+
+        // if(index == -1) return;
+
+        // for(int i = index + 1; i < nums.length ; i++){
+        //     if(nums[i] != 0){
+        //         nums[index] = nums[i];
+        //         nums[i] = 0;
+        //         index++;
+        //     }
+        // }
+
+
+
+
+
+        // int j = 0;
+        // for (int i = 1; i < nums.length; i++) {
+        //     if (nums[i] != 0) {
+        //         int temp = nums[j];
+        //         nums[j] = nums[i];
+        //         nums[i] = temp;
+        //         j++;
+        //     }
+        // }
+
+
+
+
+        int slow = 0;
+        for (int fast = 0; fast < nums.length; fast++) {
+            if (nums[fast] != 0 && nums[slow] == 0) {
+                int temp = nums[slow];
+                nums[slow] = nums[fast];
+                nums[fast] = temp;
             }
-        }
-
-        if (index == -1) return;
-
-        for (int i = index + 1; i < nums.length; i++) {
-            if (nums[i] != 0) {
-                nums[index] = nums[i];
-                nums[i] = 0;
-                index++;
+            if (nums[slow] != 0) {
+                slow++;
             }
         }
     }
