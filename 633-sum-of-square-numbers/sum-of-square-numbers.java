7b6767a67e994aeabf9b2b61e0d42d3f6998ab4a
saekrishna-k <36792759+saekrishna-k@users.noreply.github.com>
2025-05-31 16:47:15 +0530

    Time: 3 ms (94.76%) | Memory: 40.4 MB (63.02%) - LeetSync

--- 633-sum-of-square-numbers/sum-of-square-numbers.java
+++ 633-sum-of-square-numbers/sum-of-square-numbers.java
@@ -1,9 +1,23 @@
 class Solution {
 
     public boolean judgeSquareSum(int c) {
-        for (long a = 0; a * a <= c; a++) {
-            double b = Math.sqrt(c - (a * a));
-            if (b == (int) b) {
+        // for (long a = 0; a * a <= c; a++) {
+        //     double b = Math.sqrt(c - (a * a));
+        //     if (b == (int) b) {
+        //         return true;
+        //     }
+        // }
+        // return false;
+
+
+        long left = 0, right = (long) Math.sqrt(c);
+        while (left <= right) {
+            long sum = left * left + right * right;
+            if (sum < c) {
+                left++;
+            } else if (sum > c) {
+                right--;
+            } else {
                 return true;
             }
         }
