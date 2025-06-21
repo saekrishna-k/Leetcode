3b30daf0b81112838b422736ae0bcadc686e3471
saekrishna-k <36792759+saekrishna-k@users.noreply.github.com>
2025-06-21 07:43:48 +0530

    Time: 4 ms (99.14%) | Memory: 67.3 MB (52.28%) - LeetSync

--- 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.java
+++ 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.java
@@ -1,32 +1,59 @@
 class Solution {
 
     public int[] pivotArray(int[] nums, int pivot) {
-        List<Integer> lesser = new ArrayList<>();
-        List<Integer> greater = new ArrayList<>();
-        int count = 0;
+        // List<Integer> lesser = new ArrayList<>();
+        // List<Integer> greater = new ArrayList<>();
+        // int count = 0;
+
+        // for (int i : nums) {
+        //     if (i > pivot) {
+        //         greater.add(i);
+        //     } else if (i < pivot) {
+        //         lesser.add(i);
+        //     } else {
+        //         count++;
+        //     }
+        // }
+        // int index = 0;
+        // for (int i : lesser) {
+        //     nums[index++] = i;
+        // }
+
+        // for (int i = 0 ; i < count ; i++) {
+        //     nums[index++] = pivot;
+        // }
+
+        // for (int i : greater) {
+        //     nums[index++] = i;
+        // }
+
+        // return nums;
+
+        
+
+        int l = nums.length;
+        int[] lesser = new int[l];
+        int[] greater = new int[l];
+        int lc = 0, gc = 0, pc = 0;
 
         for (int i : nums) {
             if (i > pivot) {
-                greater.add(i);
+                greater[gc++] = i;
             } else if (i < pivot) {
-                lesser.add(i);
+                lesser[lc++] = i;
             } else {
-                count++;
+                pc++;
             }
         }
-        int index = 0;
-        for (int i : lesser) {
-            nums[index++] = i;
+
+        for (int i = 0; i < pc; i++) {
+            lesser[lc++] = pivot;
         }
 
-        for (int i = 0 ; i < count ; i++) {
-            nums[index++] = pivot;
+        for (int i = 0; i < gc; i++) {
+            lesser[lc++] = greater[i];
         }
 
-        for (int i : greater) {
-            nums[index++] = i;
-        }
-
-        return nums;
+        return lesser;
     }
 }
