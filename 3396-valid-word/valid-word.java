a6af9a37c9cb32e1484ed698996a682895ee6890
saekrishna-k <36792759+saekrishna-k@users.noreply.github.com>
2025-07-15 18:54:02 +0530

    Time: 1 ms (99.12%) | Memory: 41.7 MB (98.68%) - LeetSync

---
+++ 3396-valid-word/valid-word.java
@@ -0,0 +1,26 @@
+class Solution {
+
+    public boolean isValid(String word) {
+        int l = word.length();
+        if (l < 3) {
+            return false;
+        }
+
+        int vowels = 0;
+        int consonants = 0;
+
+        for (char c : word.toCharArray()) {
+            if (Character.isLetter(c)) {
+                if ("aeiouAEIOU".indexOf(c) != -1) {
+                    vowels++;
+                } else {
+                    consonants++;
+                }
+            } else if (!Character.isDigit(c)) {
+                return false;
+            }
+        }
+
+        return vowels >= 1 && consonants >= 1;
+    }
+}
