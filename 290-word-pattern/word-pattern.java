33d83842f25a9684578499a4ad41cc2067cc413a
saekrishna-k <36792759+saekrishna-k@users.noreply.github.com>
2025-07-27 14:42:39 +0530

    Time: 1 ms (79.70%) | Memory: 42.2 MB (5.35%) - LeetSync

--- 290-word-pattern/word-pattern.java
+++ 290-word-pattern/word-pattern.java
@@ -1,21 +1,22 @@
 class Solution {
+
     public boolean wordPattern(String pattern, String s) {
         String[] words = s.split(" ");
-        if (words.length != pattern.length())
-            return false;
+        if (words.length != pattern.length()) return false;
         Map<Character, String> hm = new HashMap<>();
 
         for (int i = 0; i < pattern.length(); i++) {
             char c = pattern.charAt(i);
+            String word = words[i];
             if (hm.containsKey(c)) {
-                if (!hm.get(c).equals(words[i])) {
+                if (!hm.get(c).equals(word)) {
                     return false;
                 }
             } else {
-                if (hm.containsValue(words[i])) {
+                if (hm.containsValue(word)) {
                     return false;
                 }
-                hm.put(c, words[i]);
+                hm.put(c, word);
             }
         }
         return true;
