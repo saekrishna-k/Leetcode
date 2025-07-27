f81ff521f2863e2cf8dcc0d5d3dfefefff67077e
saekrishna-k <36792759+saekrishna-k@users.noreply.github.com>
2025-07-27 11:57:22 +0530

    Time: 2 ms (73.79%) | Memory: 53.2 MB (37.97%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -19,19 +19,19 @@ class Solution {
 
         int count = 0;
         int candidate = 0;
-
+        
         for (int num : nums) {
             if (count == 0) {
                 candidate = num;
             }
-
+            
             if (num == candidate) {
                 count++;
             } else {
                 count--;
             }
         }
-
+        
         return candidate;
     }
 }
