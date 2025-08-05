4f9d9088a8490440560b5dc16a64724ce5dc8a93
saekrishna-k <36792759+saekrishna-k@users.noreply.github.com>
2025-08-05 10:05:28 +0530

    Time: 5 ms (96.95%) | Memory: 41.5 MB (72.43%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.java
+++ 647-palindromic-substrings/palindromic-substrings.java
@@ -1,26 +1,47 @@
 class Solution {
 
+    int count = 0;
     public int countSubstrings(String s) {
-        int count = 0;
+        // int count = 0;
+        // int l = s.length();
+        // for (int i = 0; i < l; i++) {
+        //     for (int j = i; j < l; j++) {
+        //         if (isPalindrome(s, i, j)) {
+        //             count++;
+        //         }
+        //     }
+        // }
+        // return count;
+
         int l = s.length();
+        if (l <= 1) {
+            return 1;
+        }
+
+        String str = s.substring(0, 1);
         for (int i = 0; i < l; i++) {
-            for (int j = i; j < l; j++) {
-                if (isPalindrome(s, i, j)) {
-                    count++;
-                }
-            }
+            expand(s, i, i);
+            expand(s, i, i + 1);
         }
         return count;
     }
 
-    private boolean isPalindrome(String s, int left, int right) {
-        while (left < right) {
-            if (s.charAt(left) != s.charAt(right)) {
-                return false;
-            }
-            left++;
-            right--;
+    private void expand(String s, int left, int right) {
+        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+            left--;
+            right++;
+            count++;
         }
-        return true;
     }
+
+    // private boolean isPalindrome(String s, int left, int right) {
+    //     while (left < right) {
+    //         if (s.charAt(left) != s.charAt(right)) {
+    //             return false;
+    //         }
+    //         left++;
+    //         right--;
+    //     }
+    //     return true;
+    // }
 }
