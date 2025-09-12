9763aca8c9883c0bfc2b5194cac293d4aec0587b
saekrishna-k <36792759+saekrishna-k@users.noreply.github.com>
2025-09-12 21:55:54 +0530

    Time: 2 ms (100.00%) | Memory: 45.7 MB (30.18%) - LeetSync

--- 3462-vowels-game-in-a-string/vowels-game-in-a-string.java
+++ 3462-vowels-game-in-a-string/vowels-game-in-a-string.java
@@ -1,12 +1,22 @@
 class Solution {
 
     public boolean doesAliceWin(String s) {
+        // for (int i = 0; i < s.length(); i++) {
+        //     char ch = s.charAt(i);
+        //     if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+        //         return true;
+        //     }
+        // }
+        // return false;
+
         for (int i = 0; i < s.length(); i++) {
-            char ch = s.charAt(i);
-            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
-                return true;
+            char c = s.charAt(i);
+            switch (c) {
+                case 'a','e','i','o','u':
+                    return true;
             }
         }
+
         return false;
     }
 }
