35acdcbd56fc73d45c2567c11e585067fa1c116c
saekrishna-k <36792759+saekrishna-k@users.noreply.github.com>
2026-06-02 17:24:51 +0530

    Time: 3 ms (98.89%) | Memory: 44.2 MB (90.19%) - LeetSync

--- 2248-minimum-cost-of-buying-candies-with-discount/minimum-cost-of-buying-candies-with-discount.java
+++ 2248-minimum-cost-of-buying-candies-with-discount/minimum-cost-of-buying-candies-with-discount.java
@@ -1,16 +1,42 @@
 class Solution {
     public int minimumCost(int[] cost) {
+        // int len = cost.length;
+
+        // Arrays.sort(cost);
+
+        // int totalCost = 0;
+
+        // for (int i = len - 1; i >= 0; i = i - 3) {
+        //     totalCost = totalCost + cost[i];
+        //     if (i - 1 >= 0) {
+        //         totalCost = totalCost + cost[i - 1];
+        //     }
+        // }
+        // return totalCost;
+
         int len = cost.length;
 
-        Arrays.sort(cost);
+        PriorityQueue<Integer> heap = new PriorityQueue<>((a, b) -> b - a);
 
         int totalCost = 0;
 
-        for (int i = len - 1; i >= 0; i = i - 3) {
-            totalCost = totalCost + cost[i];
-            if (i - 1 >= 0) {
-                totalCost = totalCost + cost[i - 1];
+        for (int item : cost) {
+            heap.add(item);
+        }
+
+        while (!heap.isEmpty()) {
+            int first = heap.poll();
+            int second = 0;
+
+            if (!heap.isEmpty()) {
+                second = heap.poll();
             }
+
+            if (!heap.isEmpty()) {
+                heap.poll();
+            }
+
+            totalCost += (first + second);
         }
         return totalCost;
     }
