2aa03db60d533675465870bd7462b00d063415d8
saekrishna-k <36792759+saekrishna-k@users.noreply.github.com>
2026-06-22 07:43:09 +0530

    Time: 2 ms (96.67%) | Memory: 43.3 MB (79.23%) - LeetSync

--- 1297-maximum-number-of-balloons/maximum-number-of-balloons.java
+++ 1297-maximum-number-of-balloons/maximum-number-of-balloons.java
@@ -1,60 +1,32 @@
 class Solution {
     public int maxNumberOfBalloons(String text) {
-    //     int b=0;
-    //     int a = 0;
-    //     int l = 0;
-    //     int o = 0;
-    //     int n = 0;
-    //     for(int  i = 0 ; i < text.length() ; i++){
-    //         switch(text.charAt(i)){
-    //             case 'b':
-    //                  b++;
-    //                  break;
-    //             case 'a':
-    //                  a++;
-    //                  break;
-    //             case 'l':
-    //                  l++;
-    //                  break;
-    //             case 'o':
-    //                  o++;
-    //                  break;   
-    //             case 'n':
-    //                  n++;
-    //                  break;                 
-    //         }
-    //     }
-    //    return Math.min(Math.min(o/2, l/2), Math.min(Math.min(b, a), n));
+        int b = 0;
+        int a = 0;
+        int l = 0;
+        int o = 0;
+        int n = 0;
 
-
-            final int[][] cache = new int[2][5];
-        cache[0][0] = 97;  // a
-        cache[0][1] = 98;  // b
-        cache[0][2] = 108; // l
-        cache[0][3] = 110; // n
-        cache[0][4] = 111; // o
-        
-        // cacl letter frequencies
-        for (char ch : text.toCharArray()) {
-            final int intValue = ch;
-            for (int i = 0; i < 5; i++) {
-                if (intValue == cache[0][i]) {
-                    cache[1][i]++;
+        for (char c : text.toCharArray()) {
+            switch (c) {
+                case 'b':
+                    b++;
                     break;
-                }
+                case 'a':
+                    a++;
+                    break;
+                case 'l':
+                    l++;
+                    break;
+                case 'o':
+                    o++;
+                    break;
+                case 'n':
+                    n++;
+                    break;
             }
         }
 
-        // l & o meet twice
-        cache[1][2] /= 2;
-        cache[1][4] /= 2;
+        return Math.min(b, Math.min(a, Math.min(n, Math.min(l / 2, o / 2))));
 
-        // find the bottleneck
-        int min = Integer.MAX_VALUE;
-        for (int frequency: cache[1]) {
-            min = Math.min(min, frequency);
-        }
-
-        return min;
     }
 }
