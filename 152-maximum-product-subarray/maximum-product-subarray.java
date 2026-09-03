66524cc4c8e1f18ba87ec8f642d15a3b9b67be54
Sai Krishna <36792759+saekrishna-k@users.noreply.github.com>
2026-09-03 19:12:50 +0530

    Time: 1 ms (93.16%) | Memory: 47.4 MB (34.14%) - LeetSync

--- 152-maximum-product-subarray/maximum-product-subarray.java
+++ 152-maximum-product-subarray/maximum-product-subarray.java
@@ -11,26 +11,45 @@ class Solution {
         // }
         // return maxProd;
 
-        int max, min, result;
-        max = min = result = nums[0];
-        int l = nums.length;
+        // int max, min, result;
+        // max = min = result = nums[0];
+        // int l = nums.length;
 
-        for (int i = 1; i < l; i++) {
-            int num = nums[i];
-            if (num < 0) {
-                // max = max + min;
-                // min = max - min;
-                // max = max - min;
+        // for (int i = 1; i < l; i++) {
+        //     int num = nums[i];
+        //     if (num < 0) {
+        //         // max = max + min;
+        //         // min = max - min;
+        //         // max = max - min;
+        //         max = max ^ min;
+        //         min = max ^ min;
+        //         max = max ^ min;
+        //     }
 
-                max = max ^ min;
-                min = max ^ min;
-                max = max ^ min;
-            }
+        //     max = Math.max(num, max * num);
+        //     min = Math.min(num, min * num);
+        //     result = Math.max(result, max);
+        // }
+        // return result;
 
-            max = Math.max(num, max * num);
-            min = Math.min(num, min * num);
-            result = Math.max(result, max);
+        
+
+        int n = nums.length, leftProduct = 1, rightProduct = 1, max = nums[0];
+
+        for(int i = 0 ; i < n ; i++){
+
+            leftProduct = leftProduct == 0 ? 1 : leftProduct;
+            rightProduct = rightProduct == 0 ? 1 : rightProduct;
+
+
+            leftProduct = leftProduct * nums[i];
+
+            rightProduct = rightProduct * nums[n - i -1];
+
+            max = Math.max(max, Math.max(leftProduct, rightProduct));
+
         }
-        return result;
+
+        return max;
     }
 }
